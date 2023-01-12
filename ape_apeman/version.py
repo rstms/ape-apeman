66b5d4ba73746a3ab488fc214bbec3ca564479c3
Matt Krueger <mkrueger@rstms.net>
2023-01-12 12:31:51 -0600

    Bump version: 0.2.1 → 0.2.2

--- ape_apeman/version.py
+++ ape_apeman/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __author__ = "Matt Krueger"
 __email__ = "mkrueger@rstms.net"
-__timestamp__ = "2022-12-02 23:36:29-06:00"
+__timestamp__ = "2023-01-12 12:31:51-06:00"
